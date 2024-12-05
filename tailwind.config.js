aaaef21e9da006f812adad38da765a19edc46900
kubilayguler <glr.kubilay45@gmail.com>
2024-12-05 20:38:42 +0300

    feat: some updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
-=======
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
->>>>>>> db9acf51d742b56cd1acc9d9fd0543d6b213cb82
   theme: {
     extend: {
       colors: {
@@ -14,7 +8,6 @@ module.exports = {
         kgold: '#FFC500',
         kwhite: '#fafafa',
       },
-<<<<<<< HEAD
       blur: {
         xs: '1px',
       },
@@ -22,18 +15,3 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/forms')],
 };
-=======
-      backgroundImage: {
-        'project1': "url('/img/project-1.png')",
-      },
-      blur: {
-        xs: '1px',
-      }
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
-
->>>>>>> db9acf51d742b56cd1acc9d9fd0543d6b213cb82
